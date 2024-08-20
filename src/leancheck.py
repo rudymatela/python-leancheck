9b43d90dc1b8c0c197f4dbb970e26f7f600d5a2b
Rudy Matela <rudy@matela.com.br>
2024-08-20 11:11:36 +0200

    remove uneeded comment...

--- src/leancheck.py
+++ src/leancheck.py
@@ -127,10 +127,6 @@ class Enumerator:
                 return cls._enumerators[c]
         except KeyError as err:
             raise TypeError(f"could not find Enumerator for {c}") from err
-        # TODO: dynamically construct enumerations
-        # if type(c) is types.GenericAlias:
-        #   use typing.get_origin(c) to get tuple      in tuple[int,bool]
-        #   use typing.get_args(c)   to get (int,bool) in tuple[int,bool]
 
 def to_tiers(xs):
     for x in xs:
