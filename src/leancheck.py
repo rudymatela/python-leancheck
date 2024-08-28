73aa97ba2da15b8eac01e6c73b4c848b7cc3cd52
Rudy Matela <rudy@matela.com.br>
2024-08-28 12:42:06 +0200

    move intercalate back into leancheck

--- src/leancheck.py
+++ src/leancheck.py
@@ -188,6 +188,32 @@ def colour_escapes():
     else:
         return '', '', '', '', ''
 
+def intercalate(generator1, generator2):
+    """
+    This function intercalates the two given iterables.
+
+    >>> list(intercalate([1,2,3], [-1, -2, -3]))
+    [1, -1, 2, -2, 3, -3]
+
+    If the arguments are generators, they will be consumed.
+
+    >>> list(intercalate((x for x in [1,2,3]), (y for y in [4,5,6])))
+    [1, 4, 2, 5, 3, 6]
+    """
+    g1 = (x for x in generator1) # makes this work on lists
+    g2 = (y for y in generator2) # makes this work on lists
+    while True:
+        try:
+            yield next(g1)
+        except StopIteration:
+            yield from g2
+            break
+        try:
+            yield next(g2)
+        except StopIteration:
+            yield from g1
+            break
+
 def check(prop, max_tests=360, verbose=True, silent=False):
     """
     Checks a property for several enumerated argument values.
