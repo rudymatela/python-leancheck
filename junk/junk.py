797cca8f04478e0531cd6cc52899b01da0e59c55
Rudy Matela <rudy@matela.com.br>
2024-08-28 12:40:37 +0200

    improve intercalate

--- junk/junk.py
+++ junk/junk.py
@@ -8,19 +8,26 @@
 # Warning: this destroys argument generators!
 def intercalate(generator1, generator2):
     """
-    This function intercalates the two given generators.
+    This function intercalates the two given iterables.
 
-    >>> list(intercalate((x for x in [1,2,3]), (y for y in [-1, -2, -3])))
+    >>> list(intercalate([1,2,3], [-1, -2, -3])
     [1, -1, 2, -2, 3, -3]
+
+    If the arguments are generators, they will be consumed.
+
+    >>> list(intercalate((x for x in [1,2,3]), (y for y in [4,5,6])))
+    [1, 4, 2, 5, 3, 6]
     """
+    g1 = (x for x in generator1) # makes this work on lists
+    g2 = (y for y in generator2) # makes this work on lists
     while True:
         try:
-            yield next(generator1)
+            yield next(g1)
         except StopIteration:
-            yield from generator2
+            yield from g2
             break
         try:
-            yield next(generator2)
+            yield next(g2)
         except StopIteration:
-            yield from generator1
+            yield from g1
             break
