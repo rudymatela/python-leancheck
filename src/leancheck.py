594faf135b87efeb38834626a0d5b1bfe2c4208b
Rudy Matela <rudy@matela.com.br>
2024-09-02 11:25:24 +0200

    src/leancheck: reorder functions

--- src/leancheck.py
+++ src/leancheck.py
@@ -60,101 +60,121 @@ import types
 import typing
 
 
-# Declaration of some internal functions.
-# These could eventually reside in a separate file,
-# but for now and for simplicity I am keeping them in a single one.
-
-def _to_tiers(xs):
-    for x in xs:
-        yield [x]
-
-def _to_list(xss):
-    for xs in xss:
-        yield from xs
-
-def _intercalate(generator1, generator2):
+def check(prop, max_tests=360, verbose=True, silent=False):
     """
-    This function intercalates the two given iterables.
+    Checks a property for several enumerated argument values.
 
-    >>> list(_intercalate([1,2,3], [-1, -2, -3]))
-    [1, -1, 2, -2, 3, -3]
+    >>> def prop_commute(x:int, y:int) -> bool:
+    ...     return x + y == y + x
+    >>> check(prop_commute)
+    +++ OK, passed 360 tests: prop_commute
+    True
 
-    If the arguments are generators, they will be consumed.
+    >>> def prop_increase(x:int, y:int) -> bool:
+    ...     return x + y > x
+    >>> check(prop_increase)
+    *** Failed! Falsifiable after 1 tests:
+        prop_increase(0, 0)
+    False
 
-    >>> list(_intercalate((x for x in [1,2,3]), (y for y in [4,5,6])))
-    [1, 4, 2, 5, 3, 6]
+    >>> def prop_sorted_twice(xs: list[int]) -> bool:
+    ...     return sorted(sorted(xs)) == sorted(xs)
+    ...
+    >>> check(prop_sorted_twice)
+    +++ OK, passed 360 tests: prop_sorted_twice
+    True
+
+    >>> def prop_sorted_len(xs: list[int]) -> bool:
+    ...     return len(sorted(xs)) == len(xs)
+    ...
+    >>> check(prop_sorted_len)
+    +++ OK, passed 360 tests: prop_sorted_len
+    True
+
+    >>> def prop_sorted_wrong(xs: list[int]) -> bool:
+    ...     return sorted(xs) == xs
+    ...
+    >>> check(prop_sorted_wrong)
+    *** Failed! Falsifiable after 6 tests:
+        prop_sorted_wrong([1, 0])
+    False
+
+    >>> check(prop_sorted_twice, silent=True)
+    True
     """
-    g1 = (x for x in generator1) # makes this work on lists
-    g2 = (y for y in generator2) # makes this work on lists
-    while True:
+    verbose = verbose and not silent
+    clear, red, green, blue, yellow = _colour_escapes()
+    sig = inspect.signature(prop)
+    ret = sig.return_annotation
+    # print(f"Property's signature: {sig}")
+    if ret != bool and not silent:
+        print(f"{yellow}Warning{clear}: property's return value is {ret} and not {bool}")
+    es = []
+    for par in sig.parameters.values():
+        # print(par.annotation)
+        e = Enumerator[par.annotation]
+        es.append(e)
+    for i, args in enumerate(itertools.islice(Enumerator.product(*es), max_tests)):
+        if not prop(*args):
+            if not silent:
+                repr_args = ', '.join(map(repr, args))
+                print(f"*** Failed! Falsifiable after {i+1} tests:")
+                print(f"    {red}{prop.__name__}{clear}({repr_args})")
+            return False
+    if verbose:
+        i = i+1
+        exhausted = " (exhausted)" if i < max_tests else ""
+        print(f"+++ OK, passed {i} tests{exhausted}: {green}{prop.__name__}{clear}")
+    return True
+
+
+def holds(prop, max_tests=360):
+    """
+    Alias to `check(prop, silent=True)`.
+
+    >>> def prop_commute(x:int, y:int) -> bool:
+    ...     return x + y == y + x
+    >>> holds(prop_commute)
+    True
+
+    >>> def prop_increase(x:int, y:int) -> bool:
+    ...     return x + y > x
+    >>> holds(prop_increase)
+    False
+    """
+    return check(prop, max_tests=max_tests, silent=True)
+
+
+def testmod(max_tests=360, silent=False, verbose=False):
+    n_failures = 0
+    n_properties = 0
+    def lineno(m):
         try:
-            yield next(g1)
-        except StopIteration:
-            yield from g2
-            break
-        try:
-            yield next(g2)
-        except StopIteration:
-            yield from g1
-            break
-
-def _zippend(*iiterables):
-    return itertools.starmap(itertools.chain,itertools.zip_longest(*iiterables, fillvalue=[]))
-
-def _pproduct(xss, yss, with_f=None):
-    if with_f is None:
-        with_f = lambda x, y: (x,y)
-    xss_ = []
-    yss_ = []
-    l = 0
-    while True:
-        xss_.append(list(next(xss, [])))
-        yss_.append(list(next(yss, [])))
-        l += 1
-        zs = []
-        for i in range(0,l):
-            zs += [with_f(x,y) for x in xss_[i] for y in yss_[l-i-1]]
-        if zs == []:
-            # This is "sound-but-incomplete".
-            # TODO: in the final version, use None as a default value
-            # in the appends above
-            # and break only in the case where we
-            # end up with empty zs because of None values
-            # there's an opportunity for memory optimization here
-            # such as in the example of product between integers and booleans
-            break
-        yield zs
-
-def _delay(xss):
-    yield []
-    yield from xss
-
-def _mmap(f,xss):
-    for xs in xss:
-        yield [f(x) for x in xs]
-
-def _llist(mkTiers):
-    yield [[]]
-    yield from _pproduct(mkTiers(), _llist(mkTiers), with_f=lambda x, xs: xs + [x])
-
-def _colour_escapes():
-    """
-    Returns colour escape sequences for clear, red, green, blue and yellow
-
-    >>> clear, red, green, blue, yellow = _colour_escapes()
-    >>> print(f"{red}This is red{clear} and {blue}this is blue{clear}.")
-    This is red and this is blue.
-    """
-    plats = ['linux'] # TODO: add other supported platforms
-    supported = sys.stdout.isatty() and sys.platform in plats
-    if supported:
-        return '\x1b[m', '\x1b[1;31m', '\x1b[32m', '\x1b[34m', '\x1b[33m'
-    else:
-        return '', '', '', '', ''
+            return m[1].__code__.co_firstlineno
+        except AttributeError:
+            return -1
+    for name, member in sorted(inspect.getmembers(sys.modules["__main__"]), key=lineno):
+        if name.startswith("prop_") and callable(member):
+            n_properties += 1
+            passed = check(member, max_tests=max_tests, silent=silent, verbose=verbose)
+            if not passed:
+                n_failures += 1
+    return (n_failures, n_properties) # just like doctest.testmod()
 
 
+def main(max_tests=360, silent=False, verbose=False, exit_on_failure=True):
+    n_failures, n_properties = testmod(max_tests=max_tests, silent=silent, verbose=verbose)
+    clear, red, green, blue, yellow = _colour_escapes()
+    if not silent:
+        if not n_properties:
+            print(f"{yellow}Warning{clear}: no properties found")
+        if n_failures:
+            print(f"\n{red}*** {n_failures} of {n_properties} properties failed{clear}")
+        elif verbose:
+            print(f"{green}+++ {n_properties} properties passed{clear}")
+    if n_failures and exit_on_failure:
+        sys.exit(1)
 
-# Actual code of LeanCheck starts here.
 
 class Enumerator:
     def __init__(self, tiers):
@@ -271,118 +291,110 @@ class Enumerator:
         except KeyError as err:
             raise TypeError(f"could not find Enumerator for {c}") from err
 
-def check(prop, max_tests=360, verbose=True, silent=False):
+
+# Declaration of some internal functions.
+# These could eventually reside in a separate file,
+# but for simplicity I am keeping them in a single one.
+
+
+def _to_tiers(xs):
+    for x in xs:
+        yield [x]
+
+
+def _to_list(xss):
+    for xs in xss:
+        yield from xs
+
+
+def _intercalate(generator1, generator2):
     """
-    Checks a property for several enumerated argument values.
+    This function intercalates the two given iterables.
 
-    >>> def prop_commute(x:int, y:int) -> bool:
-    ...     return x + y == y + x
-    >>> check(prop_commute)
-    +++ OK, passed 360 tests: prop_commute
-    True
+    >>> list(_intercalate([1,2,3], [-1, -2, -3]))
+    [1, -1, 2, -2, 3, -3]
 
-    >>> def prop_increase(x:int, y:int) -> bool:
-    ...     return x + y > x
-    >>> check(prop_increase)
-    *** Failed! Falsifiable after 1 tests:
-        prop_increase(0, 0)
-    False
+    If the arguments are generators, they will be consumed.
 
-    >>> def prop_sorted_twice(xs: list[int]) -> bool:
-    ...     return sorted(sorted(xs)) == sorted(xs)
-    ...
-    >>> check(prop_sorted_twice)
-    +++ OK, passed 360 tests: prop_sorted_twice
-    True
-
-    >>> def prop_sorted_len(xs: list[int]) -> bool:
-    ...     return len(sorted(xs)) == len(xs)
-    ...
-    >>> check(prop_sorted_len)
-    +++ OK, passed 360 tests: prop_sorted_len
-    True
-
-    >>> def prop_sorted_wrong(xs: list[int]) -> bool:
-    ...     return sorted(xs) == xs
-    ...
-    >>> check(prop_sorted_wrong)
-    *** Failed! Falsifiable after 6 tests:
-        prop_sorted_wrong([1, 0])
-    False
-
-    >>> check(prop_sorted_twice, silent=True)
-    True
+    >>> list(_intercalate((x for x in [1,2,3]), (y for y in [4,5,6])))
+    [1, 4, 2, 5, 3, 6]
     """
-    verbose = verbose and not silent
-    clear, red, green, blue, yellow = _colour_escapes()
-    sig = inspect.signature(prop)
-    ret = sig.return_annotation
-    # print(f"Property's signature: {sig}")
-    if ret != bool and not silent:
-        print(f"{yellow}Warning{clear}: property's return value is {ret} and not {bool}")
-    es = []
-    for par in sig.parameters.values():
-        # print(par.annotation)
-        e = Enumerator[par.annotation]
-        es.append(e)
-    for i, args in enumerate(itertools.islice(Enumerator.product(*es), max_tests)):
-        if not prop(*args):
-            if not silent:
-                repr_args = ', '.join(map(repr, args))
-                print(f"*** Failed! Falsifiable after {i+1} tests:")
-                print(f"    {red}{prop.__name__}{clear}({repr_args})")
-            return False
-    if verbose:
-        i = i+1
-        exhausted = " (exhausted)" if i < max_tests else ""
-        print(f"+++ OK, passed {i} tests{exhausted}: {green}{prop.__name__}{clear}")
-    return True
-
-def holds(prop, max_tests=360):
-    """
-    Alias to `check(prop, silent=True)`.
-
-    >>> def prop_commute(x:int, y:int) -> bool:
-    ...     return x + y == y + x
-    >>> holds(prop_commute)
-    True
-
-    >>> def prop_increase(x:int, y:int) -> bool:
-    ...     return x + y > x
-    >>> holds(prop_increase)
-    False
-    """
-    return check(prop, max_tests=max_tests, silent=True)
-
-def testmod(max_tests=360, silent=False, verbose=False):
-    n_failures = 0
-    n_properties = 0
-    def lineno(m):
+    g1 = (x for x in generator1) # makes this work on lists
+    g2 = (y for y in generator2) # makes this work on lists
+    while True:
         try:
-            return m[1].__code__.co_firstlineno
-        except AttributeError:
-            return -1
-    for name, member in sorted(inspect.getmembers(sys.modules["__main__"]), key=lineno):
-        if name.startswith("prop_") and callable(member):
-            n_properties += 1
-            passed = check(member, max_tests=max_tests, silent=silent, verbose=verbose)
-            if not passed:
-                n_failures += 1
-    return (n_failures, n_properties) # just like doctest.testmod()
+            yield next(g1)
+        except StopIteration:
+            yield from g2
+            break
+        try:
+            yield next(g2)
+        except StopIteration:
+            yield from g1
+            break
 
-def main(max_tests=360, silent=False, verbose=False, exit_on_failure=True):
-    n_failures, n_properties = testmod(max_tests=max_tests, silent=silent, verbose=verbose)
-    clear, red, green, blue, yellow = _colour_escapes()
-    if not silent:
-        if not n_properties:
-            print(f"{yellow}Warning{clear}: no properties found")
-        if n_failures:
-            print(f"\n{red}*** {n_failures} of {n_properties} properties failed{clear}")
-        elif verbose:
-            print(f"{green}+++ {n_properties} properties passed{clear}")
-    if n_failures and exit_on_failure:
-        sys.exit(1)
 
+def _zippend(*iiterables):
+    return itertools.starmap(itertools.chain,itertools.zip_longest(*iiterables, fillvalue=[]))
+
+
+def _pproduct(xss, yss, with_f=None):
+    if with_f is None:
+        with_f = lambda x, y: (x,y)
+    xss_ = []
+    yss_ = []
+    l = 0
+    while True:
+        xss_.append(list(next(xss, [])))
+        yss_.append(list(next(yss, [])))
+        l += 1
+        zs = []
+        for i in range(0,l):
+            zs += [with_f(x,y) for x in xss_[i] for y in yss_[l-i-1]]
+        if zs == []:
+            # This is "sound-but-incomplete".
+            # TODO: in the final version, use None as a default value
+            # in the appends above
+            # and break only in the case where we
+            # end up with empty zs because of None values
+            # there's an opportunity for memory optimization here
+            # such as in the example of product between integers and booleans
+            break
+        yield zs
+
+
+def _delay(xss):
+    yield []
+    yield from xss
+
+
+def _mmap(f,xss):
+    for xs in xss:
+        yield [f(x) for x in xs]
+
+
+def _llist(mkTiers):
+    yield [[]]
+    yield from _pproduct(mkTiers(), _llist(mkTiers), with_f=lambda x, xs: xs + [x])
+
+
+def _colour_escapes():
+    """
+    Returns colour escape sequences for clear, red, green, blue and yellow
+
+    >>> clear, red, green, blue, yellow = _colour_escapes()
+    >>> print(f"{red}This is red{clear} and {blue}this is blue{clear}.")
+    This is red and this is blue.
+    """
+    plats = ['linux'] # TODO: add other supported platforms
+    supported = sys.stdout.isatty() and sys.platform in plats
+    if supported:
+        return '\x1b[m', '\x1b[1;31m', '\x1b[32m', '\x1b[34m', '\x1b[33m'
+    else:
+        return '', '', '', '', ''
+
+
+# Runs tests if this is not being imported as a module.
 if __name__ == "__main__":
     import doctest
     import sys
