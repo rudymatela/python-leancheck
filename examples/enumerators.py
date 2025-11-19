5736b53aab9c5803cedf43137ab8b8489a8e9a6e
Rudy Matela <rudy@matela.com.br>
2025-11-19 17:39:17 +0100

    add enumerators example

---
+++ examples/enumerators.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# Example: pretty-printing some default enumerators
+#
+# (C) 2025 Rudy Matela
+# Distributed under the LGPL v2.1 or later (see the file LICENSE)
+
+from leancheck import *
+
+def pe(typ):
+    print(f">>> Enumerator[{typ}]")
+    print(Enumerator[typ])
+    print()
+
+# TODO: Enumerator.set_repr_length(12)
+
+pe(int)
+pe(bool)
+pe(list[int])
+pe(list[bool])
