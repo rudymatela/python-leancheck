17eff93cbb9079e02ba84f708039b321db7d0471
Rudy Matela <rudy@matela.com.br>
2026-02-13 10:27:21 +0100

    improve person example

--- examples/person.py
+++ examples/person.py
@@ -2,7 +2,7 @@
 #
 # Example: using LeanCheck to test a simple user class
 #
-# (C) 2025  Rudy Matela
+# (C) 2026  Rudy Matela
 # Distributed under the LGPL v2.1 or later (see the file LICENSE)
 #
 # This illustrates how one can use LeanCheck
@@ -15,14 +15,31 @@ class Person:
     name: str
     age: int
 
-    def __init__(self, name, age):
+    def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
 
     def __repr__(self):
         return f"Person('{self.name}', {self.age})"
 
+# The enumeration inferred from the constructor's argument types
+# produces spurious values as names, e.g.:
+# Person('', 0)
+# Person('aa', 0)
+# Person('', -1)
+# etc
+print(Enumerator(Person))
 
-Enumerator.register_cons(Person, str, int)
+# We can refine by registering an explicit enumerator
+# with a better selection of names and age range.
+Enumerator.register_cons(
+    Person,
+    Enumerator.choices(["Alice", "Bob", "Eve"]),
+    Enumerator.choices(range(0,99))
+)
 
+# Now test values are better:
+# Person('Alice', 2)
+# Person('Bob', 0)
+# etc...
 print(Enumerator(Person))
